27278589c563fed303b6275b0f04d364c01b7a27
avantgardener <tariktezcan2001@gmail.com>
2020-05-04 12:30:40 +0300

    Added icons, improved styling and functionality

--- app.js
+++ app.js
@@ -32,12 +32,53 @@ function get_data(lat, lon) {
     fetch(api)
         .then(response => response.json())
         .then(data => {
-            var d = new Date(data["current"]["dt"] * 1000);
+            // https://stackoverflow.com/questions/10087819/convert-date-to-another-timezone-in-javascript
+            var d = new Date().toLocaleString("tr-TR", { timeZone: data["timezone"] });
             document.getElementsByClassName("Date")[0].textContent = d.toString();
-            // document.getElementsByClassName("Location")[0].textContent = data["timezone"];
             document.getElementsByClassName("Temperature")[0].textContent = data["current"]["temp"] + " °C";
             document.getElementsByClassName("Weather")[0].textContent = data["current"]["weather"][0]["description"];
             document.getElementsByClassName("error")[0].hidden = true;
             document.getElementById("data").hidden = false;
+            change_icon(data);
         });
+}
+
+//  https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open("GET", "icons.json", true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+// https://gist.github.com/tbranyen/62d974681dea8ee0caa1
+function change_icon(data) {
+    var prefix = "wi wi-";
+    loadJSON(function (response) {
+        var weather_icons = JSON.parse(response);
+        var icon = weather_icons[data["current"]["weather"][0]["id"]].icon;
+        console.log(data);
+        var time_prefix;
+        const date = new Date(data["current"]["dt"] * 1000);
+        const sunrise = new Date(data["current"]["sunrise"] * 1000); //Convert a Unix timestamp to time
+        const sunset = new Date(data["current"]["sunset"] * 1000);
+        if (date.getHours() >= sunrise.getHours() && date.getHours() < sunset.getHours()) {
+            time_prefix = "day-";
+        }
+        else if (date.getHours() >= sunset.getHours() || date.getHours() < sunrise.getHours()) {
+            time_prefix = "night-";
+            if (icon === "sunny") {
+                icon = "clear";
+            }
+        }
+        var final_icon = prefix + time_prefix + icon;
+        console.log(final_icon);
+        document.getElementById("icon").className = final_icon;
+    });
 }
